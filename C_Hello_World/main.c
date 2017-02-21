a98313b599e7bfc8770c810c94f90bc4ae92e177
Yan Xue <xy_xueyan1982@hotmail.com>
2017-02-21 22:57:04 +0800

    Pushed from Github desktop for MAC
    For test only

---
+++ C_Hello_World/main.c
@@ -0,0 +1,34 @@
+//
+//  main.c
+//  C_Hello_World
+//
+//  Created by 薛衍 on 2016/12/16.
+//  Copyright © 2016年 薛衍. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("Hello, World Again!\n");
+    printf("1...\n");
+    printf("2...\n");
+    printf("3...\n");
+    
+    int c;
+    
+    c = getchar();
+    
+    /*while(c !=EOF){
+        
+        printf(EOF);
+        putchar(c);
+        c = getchar();
+    
+    }*/
+    
+    
+    
+}
